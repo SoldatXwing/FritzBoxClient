2ea48f687bf0a35e51b2f72051b0dad2e55ffe2c
SoldatXwing <soldatxwing@gmail.com>
2024-11-26 11:07:48 +0100

    Fixed Band converting bug from OS version 8.0

--- FritzBoxClient/Models/KnownWlanDevice.cs
+++ FritzBoxClient/Models/KnownWlanDevice.cs
@@ -1,55 +1,60 @@
-﻿using Newtonsoft.Json;
+﻿using FritzBoxClient.Converter;
+using Newtonsoft.Json;
 
-public class KnownWlanDevice
+namespace FritzBoxClient.Models
 {
-    [JsonProperty("uid")]
-    public string Uid { get; set; }
+    public class KnownWlanDevice
+    {
+        [JsonProperty("uid")]
+        public string Uid { get; set; }
 
-    [JsonProperty("name")]
-    public string Name { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
 
-    [JsonProperty("delete")]
-    public Delete Delete { get; set; }
+        [JsonProperty("delete")]
+        public Delete Delete { get; set; }
 
-    [JsonProperty("type")]
-    public string Type { get; set; }
+        [JsonProperty("type")]
+        public string Type { get; set; }
 
-    [JsonProperty("bands")]
-    public Dictionary<string, Band> Bands { get; set; }
+        [JsonProperty("bands")]
+        [JsonConverter(typeof(BandsConverter))]
+        public Dictionary<string, Band>? Bands { get; set; } = default;
 
-    [JsonProperty("ip")]
-    public string Ip { get; set; }
-}
-public class Delete
-{
-    [JsonProperty("deleteable")]
-    public bool Deleteable { get; set; }
+        [JsonProperty("ip")]
+        public string Ip { get; set; }
+    }
+    public class Delete
+    {
+        [JsonProperty("deleteable")]
+        public bool Deleteable { get; set; }
 
-    [JsonProperty("reason")]
-    public string Reason { get; set; }
-}
-public class Band
-{
-    [JsonProperty("mac")]
-    public string Mac { get; set; }
+        [JsonProperty("reason")]
+        public string Reason { get; set; }
+    }
+    public class Band
+    {
+        [JsonProperty("mac")]
+        public string Mac { get; set; }
 
-    [JsonProperty("cipher")]
-    public string Cipher { get; set; }
+        [JsonProperty("cipher")]
+        public string Cipher { get; set; }
 
-    [JsonProperty("rssi")]
-    public int Rssi { get; set; }
+        [JsonProperty("rssi")]
+        public int Rssi { get; set; }
 
-    [JsonProperty("rate")]
-    public Rate Rate { get; set; }
+        [JsonProperty("rate")]
+        public Rate Rate { get; set; }
 
-    [JsonProperty("props")]
-    public string Props { get; set; }
-}
-public class Rate
-{
-    [JsonProperty("us")]
-    public string Us { get; set; }
+        [JsonProperty("props")]
+        public string Props { get; set; }
+    }
+    public class Rate
+    {
+        [JsonProperty("us")]
+        public string Us { get; set; }
 
-    [JsonProperty("ds")]
-    public string Ds { get; set; }
+        [JsonProperty("ds")]
+        public string Ds { get; set; }
+    }
 }
